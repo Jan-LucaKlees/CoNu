8013b6068907663851553853d537f77a67c36059
Jan-Luca Klees <email@janlucaklees.de>
2019-06-02 15:26:52 +0200

    Animate removal of rows
    Rows with all cells paired up are now animated upon removal. So far they
    just collapse in height and fade out. I plan on adding a scale
    animation, so that the cells don't just get cut of, but really collapse
    visually.

--- src/Cell.jsx
+++ src/Cell.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import c from 'classnames'
 
 const Cell = ({ number, paired, selected, onClick }) =>
-	<button
-		className={ c( 'cell', { 'active': selected, 'disabled': paired } ) }
-		onClick={ onClick }>
-		{ number }
-	</button>;
+	<div className="cell-wrapper">
+		<button
+			className={ c( 'cell', { 'active': selected, 'disabled': paired } ) }
+			onClick={ onClick }>
+			{ number }
+		</button>
+	</div>
 
 export default Cell;
 
--- src/Field.jsx
+++ src/Field.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import Row from './Row'
 
 const Field = ({ state, selectedCell, onSelectCell }) =>
-	<div className="field">
-		{
-			state.rows
-				.filter( ( row ) => row.hasPairableCells() )
-				.map( ( row ) =>
+<TransitionGroup className="field">
+	{
+		state.rows
+			.filter( ( row ) => row.hasPairableCells() )
+			.map( ( row ) =>
+				<CSSTransition
+					key={ row.index }
+					timeout={ 300 }
+					classNames="row"
+				>
 					<Row
 						key={ row.index }
 						cells={ row.cells }
 						selectedCell={ selectedCell }
 						onSelectCell={ onSelectCell } />
-				)
-		}
-	</div>
+				</CSSTransition>
+			)
+	}
+</TransitionGroup>
 
 export default Field;
 
