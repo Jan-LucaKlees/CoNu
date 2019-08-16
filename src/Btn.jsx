95e6a97daf840816fd0af533230333f7f9e92602
Jan-Luca Klees <email@janlucaklees.de>
2019-08-16 12:39:11 +0200

    Touchable buttons
    The small transition implemented for making the buttons move on click now also
    works for tapping the button. This will make it nicer to use the game on mobile
    devices.

--- src/Btn.jsx
+++ src/Btn.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
 import c from 'classnames';
 
-const Btn = ( { onClick, disabled, className, children } ) => (
-	<button
-		className={ c( 'btn', className ) }
-		disabled={ disabled }
-		onClick={ onClick }>
-		{ children }
-	</button>
-);
+class Btn extends React.PureComponent {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			active: false
+		}
+	}
+	onTouchStart() {
+		if( !this.props.disabled ) {
+			this.setState({
+				active: true
+			});
+		}
+	}
+	onTouchEnd() {
+		if( !this.props.disabled ) {
+			this.setState({
+				active: false
+			});
+		}
+	}
+	render() {
+		let { onClick, disabled, className, children } = this.props;
+
+		return (
+			<button
+				className={ c( 'btn', { 'btn--active': this.state.active }, className ) }
+				disabled={ disabled }
+				onClick={ onClick }
+				onTouchStart={ () => this.onTouchStart() }
+				onTouchEnd={ () => this.onTouchEnd() } >
+				{ children }
+			</button>
+		);
+	}
+}
 
 const BtnSingleLine = ( { onClick, disabled, className, children } ) => (
 	<Btn
