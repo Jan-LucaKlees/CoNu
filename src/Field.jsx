47651d5c695d6df5e2ea38a5af4d3be9fecfb0b1
Jan-Luca Klees <email@janlucaklees.de>
2019-12-01 18:03:48 +0100

    Converted Field component to hooks
    With this commit the Field component is converted from using HOCs to using hooks
    for using the redux state. The conversion was relatively straight forward. Only
    thing that really chagned is that we are now using an immutable.js List for
    storing the active rows. (The rows that contain pariable numbers and thus need to
    be displayed.) This way we can simply provide the immutable.js `is` function as the
    second argument to the `useSelector` function for comparing the states and
    determining whether a rerender is needed. This prevents rerenders, when cells
    are paried without any rows changing. That is why the `getActiveRowIndices`
    function now returns a list, rather than an array.
    
    Furthermore, I adapted the naming of the `activeRows` variable, to refelct the
    functions name and thus be more coherent.

--- src/Field.jsx
+++ src/Field.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { List, is } from 'immutable';
 
 import * as GameLogic from './GameLogic';
 
 import Row from './Row'
 
-const Field = ( { activeRows } ) => (
-	<TransitionGroup className="field">
-		{
-			activeRows.map( rowIndex => (
-				<CSSTransition
-					key={ `row_transition_${ rowIndex }` }
-					timeout={ 300 }
-					classNames="field__row"
-				>
-					<Row key={ `row_${ rowIndex }` } index={ rowIndex } />
-				</CSSTransition>
-			))
-		}
-	</TransitionGroup>
-);
 
-const mapStateToProps = ( state ) => {
+export default () => {
+	const activeRowIndices = useSelector( getActiveRowIndices, is );
+
+	return (
+		<TransitionGroup className="field">
+			{
+				activeRowIndices.map( rowIndex => (
+					<CSSTransition
+						key={ `row_transition_${ rowIndex }` }
+						timeout={ 300 }
+						classNames="field__row"
+					>
+						<Row key={ `row_${ rowIndex }` } index={ rowIndex } />
+					</CSSTransition>
+				))
+			}
+		</TransitionGroup>
+	);
+};
+
+function getActiveRowIndices( state ) {
 	let cells = state.game.get( 'cells' );
-	let activeRows = Array.from( GameLogic.getRowIndicesWithPairableCells( cells ) );
+	let activeRowIndices = Array.from( GameLogic.getRowIndicesWithPairableCells( cells ) );
 
 	// We want to display the last row, even when it is not containing any
 	// pairable cells. This is to make the extension of the game field not
@@ -36,19 +42,14 @@ const mapStateToProps = ( state ) => {
 	// But this only applies for games that are not finished, so we check if we
 	// have any active rows in the beginning. Otherwise jsut the empty last row
 	// would be displayed together with a won message.
-	if( activeRows.length && !hasLastRowPairableCells( cells ) && !isLastRowFullWidth( cells ) ) {
+	if( activeRowIndices.length && !hasLastRowPairableCells( cells ) && !isLastRowFullWidth( cells ) ) {
 		// append the last row index to the active rows
 		let lastRowIndex = GameLogic.getRowCount( cells ) -1;
-		activeRows.push( lastRowIndex );
+		activeRowIndices.push( lastRowIndex );
 	}
-	return {
-		activeRows,
-	}
-}
 
-export default connect(
-	mapStateToProps
-)( Field )
+	return List( activeRowIndices );
+}
 
 function hasLastRowPairableCells( cells ) {
 	return GameLogic.hasRowPairableCells( cells, GameLogic.getRowCount( cells ) -1 );
