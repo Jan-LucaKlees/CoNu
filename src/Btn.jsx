8f03c6a95b304f248477f63bf1b8fa9cc8358448
Jan-Luca Klees <email@janlucaklees.de>
2019-12-01 18:00:44 +0100

    Refactored Btn components to use hooks
    The Btn component were also easily converted into a hook using, functional
    component. I really like how much lines of code this saved.
    
    As I forgot with the time now, I added comments on what the buttons are
    actually used for.
    
    Also I added memoization to all Btn components to reduce rerenders.

--- src/Btn.jsx
+++ src/Btn.jsx
@@ -1,45 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import c from 'classnames';
 
-class Btn extends React.PureComponent {
-	constructor( props ) {
-		super( props );
 
-		this.state = {
-			active: false
-		}
-	}
-	onTouchStart() {
-		if( !this.props.disabled ) {
-			this.setState({
-				active: true
-			});
-		}
-	}
-	onTouchEnd() {
-		if( !this.props.disabled ) {
-			this.setState({
-				active: false
-			});
-		}
-	}
-	render() {
-		let { onClick, disabled, className, children } = this.props;
+const Btn = React.memo( ({ onClick, disabled, className, children }) => {
+	const [ active, setActive ] = useState( false );
 
-		return (
-			<button
-				className={ c( 'btn', { 'btn--active': this.state.active }, className ) }
-				disabled={ disabled }
-				onClick={ onClick }
-				onTouchStart={ () => this.onTouchStart() }
-				onTouchEnd={ () => this.onTouchEnd() } >
-				{ children }
-			</button>
-		);
-	}
-}
+	return (
+		<button
+			className    = { c( 'btn', { 'btn--active': active }, className ) }
+			disabled     = { disabled }
+			onClick      = { onClick }
+			onTouchStart = { () => !disabled && setActive( true ) }
+			onTouchEnd   = { () => !disabled && setActive( false ) } >
+			{ children }
+		</button>
+	);
+});
 
-const BtnSingleLine = ( { onClick, disabled, className, children } ) => (
+/**
+ * Button with fixed height so only one line of text fits in
+ */
+const BtnSingleLine = React.memo( ({ onClick, disabled, className, children }) =>
 	<Btn
 		className={ c( 'btn--single-line', className ) }
 		disabled={ disabled }
@@ -48,7 +29,10 @@ const BtnSingleLine = ( { onClick, disabled, className, children } ) => (
 	</Btn>
 );
 
-const BtnInvisible = ( { onClick, disabled, className, children } ) => (
+/**
+ * Button that is not styled as such to allow for clicable images / icons.
+ */
+const BtnInvisible = React.memo( ({ onClick, disabled, className, children }) =>
 	<Btn
 		className={ c( 'btn--invisible', className ) }
 		disabled={ disabled }
@@ -57,7 +41,11 @@ const BtnInvisible = ( { onClick, disabled, className, children } ) => (
 	</Btn>
 );
 
-const BtnCuboid = ( { Front, Top, Bottom, Back, showFace, className } ) => (
+/**
+ * Animated wrapper for swapping out one button with another, all being nicely
+ * animated
+ */
+const BtnCuboid = React.memo( ({ Front, Top, Bottom, Back, showFace, className }) =>
 	<div className={ c( 'btn-cuboid', className ) } >
 		<div className={ "btn-cuboid__cuboid btn-cuboid__cuboid--show-" + showFace } >
 			<div className="btn-cuboid__face   btn-cuboid__face--front" >{ Front  }</div>
